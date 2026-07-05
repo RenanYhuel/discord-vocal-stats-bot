1c75d254794376bfe795ad8e229031d01613b390
Renan YHUEL <renan.yhuel@gmail.com>
2026-07-05 15:03:11 +0200

    fix(database): restore Drizzle instance initialization and better-sqlite3 export

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,14 @@
 import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import path from "path";
+import * as schema from "./schema";
 
 const dbPath = process.env.DATABASE_PATH || path.join(__dirname, "../../database.sqlite");
 
-const db = new Database(dbPath);
-db.pragma("journal_mode = WAL");
+const sqlite = new Database(dbPath);
+sqlite.pragma("journal_mode = WAL");
+
+const db = drizzle(sqlite, { schema });
 
 export default db;
 export { sqlite };
